4410da5a9c078d7f8d3a31c02e7884947063e1d0
杨晨 <165390625+MyrsYC@users.noreply.github.com>
2024-08-31 23:33:03 +0800

    搜索全球数据完成

--- app/src/main/java/com/example/sunnyweather/logic/Repository.kt
+++ app/src/main/java/com/example/sunnyweather/logic/Repository.kt
@@ -1,23 +1,27 @@
 package com.example.sunnyweather.logic
 
 
+import android.util.Log
 import androidx.lifecycle.liveData
-import com.example.sunnyweather.logic.model.Place
 import com.example.sunnyweather.logic.network.SunnyWeatherNetwork
 import kotlinx.coroutines.Dispatchers
 
 object Repository {
     fun searchPlaces(query: String) = liveData(Dispatchers.IO) {
         val result = try {
+            Log.d("test",query)
             val placeResponse = SunnyWeatherNetwork.searchPlaces(query)
             if (placeResponse.status == "ok") {
+                Log.d("ok", placeResponse.toString())
                 val places = placeResponse.places
                 Result.success(places)
             } else {
+                Log.d("error1", placeResponse.toString())
                 Result.failure(RuntimeException("response status is ${placeResponse.status}"))
             }
         } catch (e: Exception) {
-            Result.failure<List<Place>>(e)
+            Log.d("error2",e.toString())
+            Result.failure(e)
         }
         emit(result)
     }
---
+++ app/src/main/java/com/example/sunnyweather/ui/place/PlaceFragment.kt
@@ -0,0 +1,61 @@
+package com.example.sunnyweather.ui.place
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sunnyweather.R
+
+class PlaceFragment : Fragment() {
+    val viewModel by lazy { ViewModelProvider(this).get(PlaceViewModel::class.java) }
+    private lateinit var adapter: PlaceAdapter
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_place, container, false)
+    }
+
+    @SuppressLint("CutPasteId")
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val layoutManager = LinearLayoutManager(activity)
+        view?.findViewById<RecyclerView>(R.id.recyclerView)?.layoutManager = layoutManager
+        adapter = PlaceAdapter(this, viewModel.placeList)
+        view?.findViewById<RecyclerView>(R.id.recyclerView)?.adapter = adapter
+        view?.findViewById<EditText>(R.id.searchPlaceEdit)?.addTextChangedListener { editable ->
+            val content = editable.toString()
+            if (content.isNotEmpty()) {
+                viewModel.searchPlaces(content)
+            } else {
+                view?.findViewById<RecyclerView>(R.id.recyclerView)?.visibility = View.GONE
+                view?.findViewById<View>(R.id.bgImageView)?.visibility = View.VISIBLE
+                viewModel.placeList.clear()
+                adapter.notifyDataSetChanged()
+            }
+        }
+        viewModel.placeLiveData.observe(viewLifecycleOwner, Observer{ result ->
+            val places = result.getOrNull()
+            if (places != null) {
+                view?.findViewById<RecyclerView>(R.id.recyclerView)?.visibility = View.VISIBLE
+                view?.findViewById<View>(R.id.bgImageView)?.visibility = View.GONE
+                viewModel.placeList.clear()
+                viewModel.placeList.addAll(places)
+                adapter.notifyDataSetChanged()
+            } else {
+                Toast.makeText(activity, "未能查询到任何地点", Toast.LENGTH_SHORT).show()
+                result.exceptionOrNull()?.printStackTrace()
+            }
+        })
+    }
+}

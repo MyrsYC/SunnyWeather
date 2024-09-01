1b4347872c1bfc30da5d91c7f4857354b65b80a2
杨晨 <165390625+MyrsYC@users.noreply.github.com>
2024-09-01 16:19:09 +0800

    天气布局界面完成

---
+++ app/src/main/java/com/example/sunnyweather/ui/weather/WeatherActivity.kt
@@ -0,0 +1,17 @@
+package com.example.sunnyweather.ui.weather
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.sunnyweather.R
+
+class WeatherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+
+
+    }
+}
